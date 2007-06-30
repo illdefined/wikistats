3019b270312c525ad0700ea754a73b35baa60f4c
Mikael Voss <mikael@koeln.ccc.de>
2007-06-30 17:13:38 +0200

    Unicode is fun. ☺

--- hash.c
+++ hash.c
@@ -8,7 +8,7 @@
  * limited rights to use, publicly perform, distribute, sell, modify,
  * merge, give away, or sublicence.
  *              
- * This work is provided "AS IS" and WITHOUT WARRANTY of any kind, to
+ * This work is provided “AS IS” and WITHOUT WARRANTY of any kind, to
  * the utmost extent permitted by applicable law, neither express nor
  * implied; without malicious intent or gross negligence. In no event
  * may a licensor, author or contributor be held liable for indirect,
